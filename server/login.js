690368a8e76f2b84af4c881e4fb57022bec7ab93
yuqi <zyqandrewda@gmail.com>
2018-08-06 13:26:25 -0400

    test

--- server/login.js
+++ server/login.js
@@ -1,24 +1,37 @@
 const router = require('express').Router();
 const passport = require('./configs/passport');
 
-router.get('/', passport.authenticate('google', {
-  scope: ['profile', 'email', 'https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.readonly']
-}));
+router.get(
+  '/',
+  passport.authenticate('google', {
+    scope: [
+      'profile',
+      'email',
+      'https://www.googleapis.com/auth/calendar',
+      'https://www.googleapis.com/auth/calendar.readonly',
+    ],
+  })
+);
 
-router.get('/callback', passport.authenticate('google', {
-  failureRedirect: '/'
-}), (req, res) => {
-  res.redirect('/');
-});
+router.get(
+  '/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/',
+  }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
 
 router.get('/user', (req, res) => {
   res.send({
-    user: req.user
+    user: req.user,
   });
 });
 
 router.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
-})
+});
+
 module.exports = router;
